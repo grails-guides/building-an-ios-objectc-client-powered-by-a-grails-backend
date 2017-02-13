283a33b3739fd204c1536287a022d47d224fabf5
sdelamo <sergio.delamo@softamo.com>
2017-02-13 19:29:35 +0100

    Small copy changes

--- complete-objectivec-ios-v1/IntranetClient/GrailsFetcher.h
+++ complete-objectivec-ios-v1/IntranetClient/GrailsFetcher.h
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 
-static NSString *kServerUrl = @"http://192.168.1.41:8080";
+static NSString *kServerUrl = @"http://192.168.1.40:8080";
 static NSString *kApiVersion = @"1.0";
 static NSString *kAnnouncementsResourcePath = @"announcements";
 static NSInteger FAST_TIME_INTERVAL = 5.0;
--- complete-objectivec-ios-v2/IntranetClient/GrailsFetcher.h
+++ complete-objectivec-ios-v2/IntranetClient/GrailsFetcher.h
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 
-static NSString *kServerUrl = @"http://192.168.1.41:8080";
+static NSString *kServerUrl = @"http://192.168.1.40:8080";
 static NSString *kApiVersion = @"2.0"; // <1>
 static NSString *kAnnouncementsResourcePath = @"announcements";
 static NSInteger FAST_TIME_INTERVAL = 5.0;
